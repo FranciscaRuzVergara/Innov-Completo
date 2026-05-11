b5c5eaa018b34ee5dbe962f77aa651698158b946
FranciscaRuzVergara <fra.ruzv@duocuc.cl>
2026-05-11 15:14:47 -0400

    implementacion de bff

---
+++ bff/src/main/java/innovatech/bff/controller/ProjectTasksController.java
@@ -0,0 +1,32 @@
+package innovatech.bff.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import innovatech.bff.dto.ProjectWithTasksDTO;
+import innovatech.bff.service.ProjectTasksService;
+
+@RestController
+@RequestMapping("/bff")
+public class ProjectTasksController {
+    @Autowired
+    private ProjectTasksService projectTasksService;
+
+    @GetMapping("/{id}/tasks")
+    public ResponseEntity<ProjectWithTasksDTO> getProjectWithTasks(@PathVariable Long id){
+        try {
+            ProjectWithTasksDTO projectWithTasks = projectTasksService.findProjectWithTasks(id);
+            if(projectWithTasks==null){
+                return ResponseEntity.notFound().build();
+            }
+            return ResponseEntity.ok(projectWithTasks);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+}
---
+++ bff/src/main/java/innovatech/bff/dto/ProjectWithTasksDTO.java
@@ -0,0 +1,27 @@
+package innovatech.bff.dto;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProjectWithTasksDTO {
+    private Long projectId;
+    private String name;
+    private String description;
+    
+    @JsonFormat(pattern = "dd-MM-yyyy")
+    private LocalDate startDate;
+    
+    @JsonFormat(pattern = "dd-MM-yyyy")
+    private LocalDate endDate;
+    
+    private List<TaskDTO> tasks;
+}
