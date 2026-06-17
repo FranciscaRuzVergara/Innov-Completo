f4686c2d1062b9b477b68326780b0a81cd776ea4
Brayan Devs <107881934+Bmtqr@users.noreply.github.com>
2026-06-17 15:32:24 -0400

    ARREGLO DE ERRORES DE TEST

--- task/src/test/java/com/innovatech/task/TaskControllerTest.java
+++ task/src/test/java/com/innovatech/task/TaskControllerTest.java
@@ -5,7 +5,8 @@ import com.innovatech.task.service.TaskService;
 import org.junit.jupiter.api.Test;
 import com.innovatech.task.controller.TaskController;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -23,16 +24,18 @@ class TaskControllerTest {
     private TaskService taskService;
 
     @Test
+    @WithMockUser(username = "admin", roles = {"ADMIN"})
     void getById_ShouldReturn404_WhenTaskDoesNotExist() throws Exception {
         // Configuramos el mock para que devuelva null (tu controller maneja eso con un 404)
-        when(taskService.findById(999L)).thenReturn(null);
+        when(taskService.findById(99L)).thenReturn(null);
 
-        mockMvc.perform(get("/tasks/999"))
+        mockMvc.perform(get("/tasks/99"))
                 .andExpect(status().isNotFound())
                 .andExpect(content().string("A task with this ID does not exist."));
     }
 
     @Test
+    @WithMockUser(username = "admin", roles = {"ADMIN"})
     void getAll_ShouldReturnOk() throws Exception {
         mockMvc.perform(get("/tasks/all"))
                 .andExpect(status().isOk());
